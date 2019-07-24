9fe7b6640ba4cfb9cd9607532f974872ba79aff7
Andrey <andregunkin90@gmail.com>
2019-07-24 18:58:40 +0300

    Add error message

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,6 +4,7 @@ import SwapiService from '../../services/swapi-service'
 
 import './random-planet.css'
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator/error-indicator';
 
 export default class RandomPlanet extends Component {
 
@@ -11,7 +12,8 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true     
+        loading: true,
+        error: false  
     };
 
     constructor() {
@@ -25,19 +27,32 @@ export default class RandomPlanet extends Component {
             loading: false
         });
     };
-
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
     updatePlanet() {
-        const id = 12;
-        this.swapiService.getPlanet(id).then(this.onPlanetLoaded);
+        const id = 1511;
+        this.swapiService.getPlanet(id)
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
     };
 
     render() {
+        const { planet, loading, error } = this.state;
 
-        const { planet, loading } = this.state;
+        const hasData = !(loading || error);
+
+        const errorMessage = error ? <ErrorIndicator /> : null;
         const spinner = loading ? <Spinner /> : null;
-        const planetView = loading ? null : <PlanetView planet={planet}/>
+        const planetView = hasData ? <PlanetView planet={planet}/>: null
+        
+
         return(
             <div className="random-planet">
+                {errorMessage}
                 {spinner}
                 {planetView}
             </div>
