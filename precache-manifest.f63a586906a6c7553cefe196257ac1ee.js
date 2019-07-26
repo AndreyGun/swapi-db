4c34c69b7590997f004a6e435cf265554d64df25
Andrey <andregunkin90@gmail.com>
2019-07-26 18:28:52 +0300

    Updates

---
+++ precache-manifest.f63a586906a6c7553cefe196257ac1ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ed6ac3de895d91e7dbaccb85c46fdd",
+    "url": "/AndreyGun/swapi-db.git/index.html"
+  },
+  {
+    "revision": "b45d2b53475df7b8f16d",
+    "url": "/AndreyGun/swapi-db.git/static/css/main.fee1339b.chunk.css"
+  },
+  {
+    "revision": "776c2f533a9ce0ee7fb6",
+    "url": "/AndreyGun/swapi-db.git/static/js/2.3129f4d1.chunk.js"
+  },
+  {
+    "revision": "b45d2b53475df7b8f16d",
+    "url": "/AndreyGun/swapi-db.git/static/js/main.ce41514b.chunk.js"
+  },
+  {
+    "revision": "62e601072cc0d731ece6",
+    "url": "/AndreyGun/swapi-db.git/static/js/runtime~main.84abe5bb.js"
+  }
+]);
